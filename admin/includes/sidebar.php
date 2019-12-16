4cb4f9bfe52d3781e23bc35ce5b52cd543f2b5ff
gibbo jean <emric.gibbs@gmail.com>
2019-12-15 21:35:23 -0500

    sidebar #1 correction

--- admin/includes/sidebar.php
+++ admin/includes/sidebar.php
@@ -14,11 +14,11 @@
             
                 <ul class="sidebar-menu collapsible collapsible-accordion" data-collapsible="accordion">
                     <li class="no-padding"><a class="waves-effect waves-grey" href="dashboard.php">
-                        <i class="material-icons">settings_input_svideo</i>Dashboard</a></li>
+                        <i class="material-icons"></i>Dashboard</a></li>
                     <li class="no-padding">
                         <a class="collapsible-header waves-effect waves-grey"><i class="material-icons">
-                            apps</i>Department<i class="nav-drop-icon material-icons">
-                                keyboard_arrow_right</i></a>
+                            </i>Department<i class="nav-drop-icon material-icons">
+                                </i></a>
                         <div class="collapsible-body">
                             <ul>
                                 <li><a href="adddepartment.php">Add Department</a></li>
@@ -29,7 +29,7 @@
                     <li class="no-padding">
                         <a class="collapsible-header waves-effect waves-grey"><i class="material-icons">
                             code</i>Leave Type<i class="nav-drop-icon material-icons">
-                                keyboard_arrow_right</i></a>
+                                </i></a>
                         <div class="collapsible-body">
                             <ul>
                                 <li><a href="addleavetype.php">Add Leave Type</a></li>
@@ -40,7 +40,7 @@
                     <li class="no-padding">
                         <a class="collapsible-header waves-effect waves-grey"><i class="material-icons">
                             account_box</i>Employees<i class="nav-drop-icon material-icons">
-                                keyboard_arrow_right</i></a>
+                                </i></a>
                         <div class="collapsible-body">
                             <ul>
                                 <li><a href="addemployee.php">Add Employee</a></li>
@@ -52,8 +52,8 @@
 
    <li class="no-padding">
                         <a class="collapsible-header waves-effect waves-grey"><i class="material-icons">
-                            desktop_windows</i>Leave Management<i class="nav-drop-icon material-icons">
-                                keyboard_arrow_right</i></a>
+                            </i>Leave Management<i class="nav-drop-icon material-icons">
+                                </i></a>
                         <div class="collapsible-body">
                             <ul>
                                 <li><a href="leaves.php">All Leaves </a></li>
@@ -67,11 +67,11 @@
 
 
   <li class="no-padding"><a class="waves-effect waves-grey" href="changepassword.php">
-      <i class="material-icons">settings_input_svideo</i>Change Password</a></li>
+      <i class="material-icons"></i>Change Password</a></li>
 
                         <li class="no-padding">
                                 <a class="waves-effect waves-grey" href="logout.php">
-                                    <i class="material-icons">exit_to_app</i>Sign Out</a>
+                                    <i class="material-icons"></i>Sign Out</a>
                             </li>  
                  
 
