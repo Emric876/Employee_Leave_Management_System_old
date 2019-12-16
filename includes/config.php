f2c6bd54724eccac188ff330e5210eba52663a80
gibbo jean <emric.gibbs@gmail.com>
2019-12-15 20:49:26 -0500

    remote dp config

--- includes/config.php
+++ includes/config.php
@@ -1,10 +1,17 @@
 <?php 
 // DB credentials.
-define('DB_HOST','localhost');
-define('DB_USER','root');
-define('DB_PASS','');
-define('DB_NAME','elms');
+
+// define('DB_HOST','localhost');
+// define('DB_USER','root');
+// define('DB_PASS','');
+// define('DB_NAME','elms');
+
 // Establish database connection.
+define('DB_HOST','remotemysql.com');
+define('DB_USER','wFK8rlm1sw');
+define('DB_PASS','tNPYYfhPUk');
+define('DB_NAME','wFK8rlm1sw');
+
 try
 {
 $dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
